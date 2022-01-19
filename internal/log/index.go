d329dec9203d7c59b0da2b7fac4dfafbf9116e9e
Kolybelnikov, Andrey <andrey@gatech.edu>
2022-01-19 14:41:05 +0100

    Segment and test

--- internal/log/index.go
+++ internal/log/index.go
@@ -10,7 +10,7 @@ import (
 var (
 	offWidth uint64 = 4
 	posWidth uint64 = 8
-	endWidth        = offWidth + posWidth
+	entWidth        = offWidth + posWidth
 )
 
 type index struct {
@@ -59,26 +59,26 @@ func (i *index) Read(in int64) (out uint32, pos uint64, err error) {
 		return 0, 0, io.EOF
 	}
 	if in == -1 {
-		out = uint32((i.size / endWidth) - 1)
+		out = uint32((i.size / entWidth) - 1)
 	} else {
 		out = uint32(in)
 	}
-	pos = uint64(out) * endWidth
-	if i.size < pos+endWidth {
+	pos = uint64(out) * entWidth
+	if i.size < pos+entWidth {
 		return 0, 0, io.EOF
 	}
 	out = enc.Uint32(i.mmap[pos : pos+offWidth])
-	pos = enc.Uint64(i.mmap[pos+offWidth : pos+endWidth])
+	pos = enc.Uint64(i.mmap[pos+offWidth : pos+entWidth])
 	return out, pos, nil
 }
 
 func (i *index) Write(off uint32, pos uint64) error {
-	if uint64(len(i.mmap)) < i.size+endWidth {
+	if uint64(len(i.mmap)) < i.size+entWidth {
 		return io.EOF
 	}
 	enc.PutUint32(i.mmap[i.size:i.size+offWidth], off)
-	enc.PutUint64(i.mmap[i.size+offWidth:i.size+endWidth], pos)
-	i.size += uint64(endWidth)
+	enc.PutUint64(i.mmap[i.size+offWidth:i.size+entWidth], pos)
+	i.size += uint64(entWidth)
 	return nil
 }
 
