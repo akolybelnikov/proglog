73777dea6bdb6143de65b88e9cdaf3a3ef097f83
akolybelnikov <a.kolybelnikov@gmail.com>
2022-06-06 00:40:52 +0200

    Mutual auth with client TLS

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -44,7 +44,9 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	require.NoError(t, err)
 
 	clientTLSConfig, err := config.SetUpTLSConfig(config.TLSConfig{
-		CAFile: config.CAFile,
+		CertFile: config.ClientCertFile,
+		KeyFile:  config.ClientKeyFile,
+		CAFile:   config.CAFile,
 	})
 	require.NoError(t, err)
 
@@ -60,6 +62,7 @@ func setupTest(t *testing.T, fn func(*Config)) (
 		KeyFile:       config.ServerKeyFile,
 		CAFile:        config.CAFile,
 		ServerAddress: l.Addr().String(),
+		Server:        true,
 	})
 	require.NoError(t, err)
 	serverCreds := credentials.NewTLS(serverTLSConfig)
